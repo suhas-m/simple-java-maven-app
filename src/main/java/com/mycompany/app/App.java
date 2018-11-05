32d1d9093b1943d5cbb20b94d17c27cd76214fb5
Suhas Mhatre <suhas.mhatre@bridgelabz.com>
2018-11-05 18:07:37 +0530

    Changed message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! 123";
+    private final String message = "Hello World! 123 4";
 
     public App() {}
 
